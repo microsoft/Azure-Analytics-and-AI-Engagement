856ec7fdfe1bd7e28f65730cff3de32080d0a70f
Daron Yondem <daron@yondem.com>
2020-05-24 18:13:59 +0300

    Removing Storage Account Credentials

--- artifacts/environment-setup/sql/02-load-csv.sql
+++ artifacts/environment-setup/sql/02-load-csv.sql
@@ -2,5 +2,4 @@ COPY INTO #TABLE_NAME#
 FROM 'https://solliancepublicdata.blob.core.windows.net/cdp/data/#CSV_FILE_NAME#.csv'
 WITH (
 	FILE_TYPE = 'CSV'
-	,CREDENTIAL=(IDENTITY= 'Storage Account Key', SECRET='c/5jTocejppX2bd7luchLSv8f8k8J4ickv5Gwm+Nv45kn2IMFPGSeHbFHbXEUxrNmvweFLy01+A+omn0q9uHOQ==')
 )
