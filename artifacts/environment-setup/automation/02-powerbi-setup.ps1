9013152d3ab77d7e4d2b9dcf8c67ced89903ecf4
Daron Yondem <daron@yondem.com>
2020-06-02 16:15:18 +0300

    Completed PowerBI File uploads

--- artifacts/environment-setup/automation/02-powerbi-setup.ps1
+++ artifacts/environment-setup/automation/02-powerbi-setup.ps1
@@ -6,21 +6,26 @@ $InformationPreference = "Continue"
 # TODO: Keep all required configuration in C:\LabFiles\AzureCreds.ps1 file
 . C:\LabFiles\AzureCreds.ps1
 
-Install-Module -Name MicrosoftPowerBIMgmt
-Install-Module -Name MicrosoftPowerBIMgmt.Profile
+$userName = $AzureUserName                                              # READ FROM FILE
+$password = $AzurePassword | ConvertTo-SecureString -asPlainText -Force # READ FROM FILE
+
+#Install-Module -Name MicrosoftPowerBIMgmt
+#Install-Module -Name MicrosoftPowerBIMgmt.Profile
 
 Import-Module MicrosoftPowerBIMgmt
 Import-Module MicrosoftPowerBIMgmt.Profile
 
-$userName = $AzureUserName                                              # READ FROM FILE
-$password = $AzurePassword | ConvertTo-SecureString -asPlainText -Force # READ FROM FILE
-$credential = New-Object System.Management.Automation.PSCredential($userName, $password)
+# PowerBI Connection
+Write-Information "Connecting to PowerBI Service"
+$credentialForPowerBI = New-Object System.Management.Automation.PSCredential($userName, $password)
+Connect-PowerBIServiceAccount -Credential $credentialForPowerBI
 
-Connect-PowerBIServiceAccount -Credential $credential
+Write-Information "Creating PowerBI Workspace"
+$newPowerBIWorkSpace = New-PowerBIWorkspace -Name "ASA-EXP"
 
 Write-Information "Uploading PowerBI Reports"
-
-New-PowerBIReport -Path ".\artifacts\exports\powerbi\1. CDP Vision Demo.pbix" -Name "1. CDP Vision Demo" -ConflictAction CreateOrOverwrite
+New-PowerBIReport -Path ".\artifacts\exports\powerbi\1. CDP Vision Demo.pbix" -Name "1-CDP Vision Demo" -ConflictAction CreateOrOverwrite -WorkspaceId $newPowerBIWorkSpace.id
+New-PowerBIReport -Path ".\artifacts\exports\powerbi\2. Billion Rows Demo.pbix" -Name "2-Billion Rows Demo.pbix" -ConflictAction CreateOrOverwrite -WorkspaceId $newPowerBIWorkSpace.id
 
 # Invoke-PowerBIRestMethod -Url 'groups/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/datasets/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/tables/xxxxxx/rows' -Method Delete
 
