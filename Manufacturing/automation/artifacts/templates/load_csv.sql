c2fd08c3aed9ae8da39202e817524459078ad700
xor-ranavikrant <ranavikrant.chouhan@xoriant.com>
2020-08-29 00:30:01 +0530

    Fix for identity issue in CampaignData table

--- Manufacturing/automation/artifacts/templates/load_csv.sql
+++ Manufacturing/automation/artifacts/templates/load_csv.sql
@@ -1,6 +1,8 @@
+SET IDENTITY_INSERT dbo.CampaignData ON
 COPY INTO [#TABLE_NAME#]
 FROM 'https://dreamdemostrggen2r16gxwb.blob.core.windows.net/customcsv/Manufacturing B2B Scenario Dataset/#CSV_FILE_NAME#.csv'
 WITH (
 	FILE_TYPE = 'CSV',
 	FIRSTROW = #DATA_START_ROW_NUMBER# 
 )
+SET IDENTITY_INSERT dbo.CampaignData OFF
