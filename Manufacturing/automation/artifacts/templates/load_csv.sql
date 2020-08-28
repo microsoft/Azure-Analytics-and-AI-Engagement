35df28fcd5c09f38f082b0aced2b61d374eadd40
xor-ranavikrant <ranavikrant.chouhan@xoriant.com>
2020-08-28 15:54:28 +0530

    updated dataset file and added sql file for csv transfer

---
+++ Manufacturing/automation/artifacts/templates/load_csv.sql
@@ -0,0 +1,6 @@
+COPY INTO #TABLE_NAME# 
+FROM 'https://dreamdemostrggen2r16gxwb.blob.core.windows.net/customcsv/Manufacturing B2B Scenario Dataset/#CSV_FILE_NAME#.csv'
+WITH (
+	FILE_TYPE = 'CSV',
+	FIRSTROW = #DATA_START_ROW_NUMBER# 
+)
