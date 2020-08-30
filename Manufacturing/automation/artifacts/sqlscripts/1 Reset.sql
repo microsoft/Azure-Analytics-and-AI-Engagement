dd7b9bf8a222528789b6343faaa29ceb99b28cc3
xor-ranavikrant <ranavikrant.chouhan@xoriant.com>
2020-08-30 12:03:12 +0530

    making customcsv container public for reset script

--- Manufacturing/automation/artifacts/sqlscripts/1 Reset.sql
+++ Manufacturing/automation/artifacts/sqlscripts/1 Reset.sql
@@ -5,31 +5,31 @@ Delete from Campaignproducts;
 Delete from sales;
 
 COPY INTO [dbo].[CampaignData_Bubble]
-FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/mfg-pocdata/Before/CampaignData_Bubble.csv'
+FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/customcsv/Manufacturing B2B Scenario Dataset/CampaignData_Bubble.csv'
 WITH (
     FILE_TYPE = 'CSV',
     FIRSTROW = 2
 )
 COPY INTO [dbo].[CampaignData]
-FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/mfg-pocdata/Before/CampaignData.csv'
+FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/customcsv/Manufacturing B2B Scenario Dataset/CampaignData.csv'
 WITH (
 	FILE_TYPE = 'CSV',
 	FIRSTROW = 2 
 )
 COPY INTO [dbo].[Product]
-FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/mfg-pocdata/Before/Product.csv'
+FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/customcsv/Manufacturing B2B Scenario Dataset/Product.csv'
 WITH (
 	FILE_TYPE = 'CSV',
 	FIRSTROW = 2 
 )
 COPY INTO [dbo].[Campaignproducts]
-FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/mfg-pocdata/Before/Campaignproducts.csv'
+FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/customcsv/Manufacturing B2B Scenario Dataset/Campaignproducts.csv'
 WITH (
 	FILE_TYPE = 'CSV',
 	FIRSTROW = 2 
 )
 COPY INTO [dbo].[Sales]
-FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/mfg-pocdata/Before/sales.csv'
+FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/customcsv/Manufacturing B2B Scenario Dataset/sales.csv'
 WITH (
 	FILE_TYPE = 'CSV',
 	FIRSTROW = 2 
--- Manufacturing/automation/artifacts/templates/load_csv.sql
+++ Manufacturing/automation/artifacts/templates/load_csv.sql
@@ -1,6 +1,6 @@
 
 COPY INTO [#TABLE_NAME#]
-FROM 'https://dreamdemostrggen2r16gxwb.blob.core.windows.net/customcsv/Manufacturing B2B Scenario Dataset/#CSV_FILE_NAME#.csv'
+FROM 'https://#STORAGE_ACCOUNT#.blob.core.windows.net/customcsv/Manufacturing B2B Scenario Dataset/#CSV_FILE_NAME#.csv'
 WITH (
 	FILE_TYPE = 'CSV',
 	FIRSTROW = #DATA_START_ROW_NUMBER# 
