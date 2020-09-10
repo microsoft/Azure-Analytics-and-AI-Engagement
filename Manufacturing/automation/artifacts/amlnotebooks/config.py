7ea4616417f6f0430c2d59437019c8af0e565f25
xor-ranavikrant <ranavikrant.chouhan@xoriant.com>
2020-09-10 17:43:56 +0530

    Notebooks and ML workspace code added

---
+++ Manufacturing/automation/artifacts/amlnotebooks/config.py
@@ -0,0 +1,37 @@
+# Workspace config
+subscription_id=''
+resource_group=''
+workspace_name=''
+
+# External Storage Account
+STORAGE_ACCOUNT_NAME = ""
+STORAGE_ACCOUNT_ACCESS_KEY = ""
+
+# 3a
+HARD_HAT_POST_URL = ""
+HARD_HAT_HEADERS = {'Prediction-Key': "","Content-Type":"application/json"}
+
+# 3b 
+WELDING_HELMET_POST_URL = ""
+WELDING_HELMET_HEADERS = {'Prediction-Key': "","Content-Type":"application/json"}
+
+# 3c
+FACE_MASK_POST_URL = ""
+FACE_MASK_HEADERS = {'Prediction-Key': "","Content-Type":"application/json"}
+
+# 3d
+QUALITY_CONTROL_POST_URL = ""
+QUALITY_CONTROL_HEADERS = {'Prediction-Key': "","Content-Type":"application/json"}
+
+# 4a
+
+FORM_RECOGNIZER_ENDPOINT = ""
+# Change if api key is expired
+FORM_RECOGNIZER_APIM_KEY = ""
+# This model is the one trained on 5 forms
+FORM_RECOGNIZER_MODEL_ID = ""
+
+# 4b
+
+TRANLATION_URL = ""
+TRANLATION_HEADERS = {'Ocp-Apim-Subscription-Key': "","Content-Type":"application/json; charset=UTF-8"}
--- Manufacturing/automation/phase2.ps1
+++ Manufacturing/automation/phase2.ps1
@@ -39,3 +39,57 @@ foreach($project in $projects)
 	$Result = Invoke-RestMethod -Uri $url -Method POST -Body $body -ContentType "application/json" -Headers @{"Training-key"="$($destinationKey)"}
 	
 }
+
+
+#AML Workspace
+#create aml workspace
+az extension add -n azure-cli-ml
+
+$amlworkspacename = "amlws-$suffix"
+az ml workspace create -w $amlworkspacename -g $rgName
+az ml computetarget create aks --name  "new-aks" --resource-group $rgName --workspace-name $amlWorkSpaceName 
+
+#attach a folder to set resouce group and workspace name (to skip passing ws and rg in calls after this line)
+az ml folder attach -w $amlworkspacename -g $rgName
+
+#create and delete a compute instance to get the code folder created in default store
+az ml computetarget create computeinstance -n cpuShell -s "STANDARD_D3_V2" -v
+#az ml computetarget delete -n cpuShell -v
+
+#get default data store
+$defaultdatastore = az ml datastore show-default --resource-group $rgName --workspace-name $amlworkspacename --output json | ConvertFrom-Json
+$defaultdatastoreaccname = $defaultdatastore.account_name
+
+#get fileshare and code folder within that
+$storageAcct = Get-AzStorageAccount -ResourceGroupName $rgName -Name $defaultdatastoreaccname
+$share = Get-AzStorageShare -Prefix 'code' -Context $storageAcct.Context 
+$shareName = $share.Name
+$shareName
+
+#create Users folder ( it wont be there unless we launch the workspace in UI)
+New-AzStorageDirectory -Context $storageAcct.Context -ShareName $shareName -Path "Users"
+
+#copy notebooks to ml workspace
+$notebooks=Get-ChildItem "./artifacts/amlnotebooks" | Select BaseName
+foreach($notebook in $notebooks)
+{
+	if($notebook.BaseName -eq "config")
+	{
+		$source="./artifacts/amlnotebooks/"+$notebook.BaseName+".py"
+		$path="/Users/"+$notebook.BaseName+".py"
+	}
+	else
+	{
+		$source="./artifacts/amlnotebooks/"+$notebook.BaseName+".ipynb"
+		$path="/Users/"+$notebook.BaseName+".ipynb"
+	}
+
+Set-AzStorageFileContent `
+   -Context $storageAcct.Context `
+   -ShareName $shareName `
+   -Source $source `
+   -Path $path
+}
+   
+az ml computetarget delete -n cpuShell -v
+
