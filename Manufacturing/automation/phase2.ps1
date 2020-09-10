2821775cab14391e5d5a4bcc73edfd37365d4fe6
xor-ranavikrant <ranavikrant.chouhan@xoriant.com>
2020-09-10 21:30:42 +0530

    Update phase2.ps1

--- Manufacturing/automation/phase2.ps1
+++ Manufacturing/automation/phase2.ps1
@@ -1,9 +1,7 @@
 #Custom Vision 
 pip install -r ./artifacts/copyCV/requirements.txt
 $key=az cognitiveservices account keys list --name $customVisionName -g $rgName|ConvertFrom-json
-$key=$key.key1
-
-$destinationKey=$key
+$destinationKey=$key.key1
 $sourceKey="7f743d4b8d6d459fb2bb0e8648dfa38e"  #todo: find a way to get this securely
 $sourceProjectId="b2e4f4ce-d9f1-4fb7-aa0c-2f50fdc14d1b"
 $destinationregion= "https://$($location).api.cognitive.microsoft.com"
@@ -11,6 +9,8 @@ $sourceregion= "https://westus2.api.cognitive.microsoft.com"
 python ./artifacts/copyCV/migrate_project.py -p $sourceProjectId -s $sourceKey -se $sourceregion -d $destinationKey -de $destinationregion
 $url = "https://$($location).api.cognitive.microsoft.com/customvision/v3.2/training/projects"
 $projects = Invoke-RestMethod -Uri $url -Method GET  -ContentType "application/json" -Headers @{ "Training-key"="$($destinationKey)" };
+$resource=az resource show -g $rgName -n $customVisionName --resource-type "Microsoft.CognitiveServices/accounts"|ConvertFrom-Json
+$resourceId=$resource.id
 foreach($project in $projects)
 {
 	$projectId=$project.id
@@ -18,9 +18,9 @@ foreach($project in $projects)
 	$url = "https://$($location).api.cognitive.microsoft.com/customvision/v3.2/training/projects/$($project.id)/tags"
 	$tags = Invoke-RestMethod -Uri $url -Method GET  -ContentType "application/json" -Headers @{ "Training-key"="$($destinationKey)" };
 	$tagList = New-Object System.Collections.ArrayList
-	foreach(tag in tags)
+	foreach($tag in $tags)
 	{
-		$tagList.Add(tag.id)
+		$tagList.Add($tag.id)
 	}
 	$body = "{
       `"selectedTags`": []
@@ -34,7 +34,7 @@ foreach($project in $projects)
 	$url="https://$($location).api.cognitive.microsoft.com/customvision/v3.3/Training/projects/$($projectId)/iterations"
 	$iterations=Invoke-RestMethod -Uri $url -Method GET  -ContentType "application/json" -Headers @{ "Training-key"="$($destinationKey)" };
 	$iterationId=$iterations[0].id
-	$url="https://$($location).api.cognitive.microsoft.com/customvision/v3.3/Training/projects/$($projectId)/iterations/$($iterationId)/publish?publishName=$($projectName)&predictionId=$predictionId"  #TODO Prediction ID
+	$url="https://$($location).api.cognitive.microsoft.com/customvision/v3.3/Training/projects/$($projectId)/iterations/$($iterationId)/publish?publishName=$($projectName)&predictionId=$($resourceId)"  #TODO Prediction ID
 	$body = "{}"
 	$Result = Invoke-RestMethod -Uri $url -Method POST -Body $body -ContentType "application/json" -Headers @{"Training-key"="$($destinationKey)"}
 	
@@ -49,7 +49,7 @@ $amlworkspacename = "amlws-$suffix"
 az ml workspace create -w $amlworkspacename -g $rgName
 az ml computetarget create aks --name  "new-aks" --resource-group $rgName --workspace-name $amlWorkSpaceName 
 
-#attach a folder to set resouce group and workspace name (to skip passing ws and rg in calls after this line)
+#attach a folder to set resource group and workspace name (to skip passing ws and rg in calls after this line)
 az ml folder attach -w $amlworkspacename -g $rgName
 
 #create and delete a compute instance to get the code folder created in default store
