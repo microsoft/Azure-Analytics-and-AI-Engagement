fa0a7cf726d98fb5e2f5e25258cdcff15b8d1843
priyankamali <Priyanka.Mali@Xoriant.com>
2020-09-10 10:49:42 +0530

    Updated script, sqluser and tableschema for MFG Dynamic Data Masking reports data

--- Manufacturing/automation/artifacts/sqlscripts/tableschema.sql
+++ Manufacturing/automation/artifacts/sqlscripts/tableschema.sql
@@ -1110,6 +1110,181 @@ WITH
 	DISTRIBUTION = HASH ( [UnplannedMaintenanceActivityId] ),
 	CLUSTERED COLUMNSTORE INDEX
 )
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Role]
+(
+	[RoleID] [int] NOT NULL,
+	[Name] [varchar](100) NOT NULL,
+	[Email] [varchar](100) NULL,
+	[Roles] [varchar](128) NULL
+)
+WITH
+(
+	DISTRIBUTION = ROUND_ROBIN,
+	CLUSTERED COLUMNSTORE INDEX
+)
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Campaign_Analytics_New]
+(
+	[Region] [varchar](50) NULL,
+	[Country] [varchar](50) NULL,
+	[Product_Category] [varchar](50) NULL,
+	[Campaign_Name] [varchar](50) NULL,
+	[Revenue] [varchar](50) NULL,
+	[Revenue_Target] [varchar](50) NULL,
+	[RoleID] [int] NULL,
+	[City] [nvarchar](100) NULL
+)
+WITH
+(
+	DISTRIBUTION = ROUND_ROBIN,
+	CLUSTERED COLUMNSTORE INDEX
+)
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Campaign_Analytics]
+(
+	[Region] [varchar](50) NULL,
+	[Country] [varchar](50) NULL,
+	[Product_Category] [varchar](50) NULL,
+	[Campaign_Name] [varchar](50) NULL,
+	[Revenue] [varchar](50) NULL,
+	[Revenue_Target] [varchar](50) NULL,
+	[City] [varchar](50) NULL,
+	[State] [varchar](50) NULL
+)
+WITH
+(
+	DISTRIBUTION = ROUND_ROBIN,
+	CLUSTERED COLUMNSTORE INDEX
+)
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[CLS_DAM_AC_New] AS 
+GRANT SELECT ON Campaign_Analytics([Region],[Country],[Product_Category],[Campaign_Name],[Revenue_Target],[City],[State]) TO SalesStaff;
+EXECUTE AS USER ='SalesStaff'
+select [Region],[Country],[Product_Category],[Campaign_Name],[Revenue_Target],[City],[State] from Campaign_Analytics
+GO
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[CLS_DAM_F_New] AS 
+BEGIN TRY
+-- Generate a divide-by-zero error  
+	
+		GRANT SELECT ON Campaign_Analytics([Region],[Country],[Product_Category],[Campaign_Name],[Revenue_Target],[CITY],[State]) TO SalesStaff;
+		EXECUTE AS USER ='SalesStaff'
+		select * from Campaign_Analytics
+END TRY
+BEGIN CATCH
+	SELECT
+		ERROR_NUMBER() AS ErrorNumber,
+		ERROR_STATE() AS ErrorState,
+		ERROR_SEVERITY() AS ErrorSeverity,
+		ERROR_PROCEDURE() AS ErrorProcedure,
+		
+		ERROR_MESSAGE() AS ErrorMessage;
+END CATCH;
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[CLS_CEONew] AS 
+Revert;
+GRANT SELECT ON Campaign_Analytics TO InventoryManager;  --Full access to all columns.
+-- Step:6 Let us check if our CEO user can see all the information that is present. Assign Current User As 'CEO' and the execute the query
+EXECUTE AS USER ='InventoryManager'
+select * from Campaign_Analytics
+GO
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[Sp_MFGRLS] AS 
+Begin  
+       -- After creating the users, read access is provided to all three users on FactSales table
+       GRANT SELECT ON FactSales TO InventoryManager, SalesStaffMiami, SalesStaffSanDiego;  
+
+       IF EXISts (SELECT 1 FROM sys.security_predicates sp where sp.predicate_definition='([Security].[fn_securitypredicate]([SalesRep]))')
+       BEGIN
+              DROP SECURITY POLICY SalesFilter;
+              DROP FUNCTION Security.fn_securitypredicate;
+       END
+       
+       IF  EXISTS (SELECT * FROM sys.schemas where name='Security')
+       BEGIN  
+       DROP SCHEMA Security;
+       End
+       
+       /* Moving ahead, we Create a new schema, and an inline table-valued function. 
+       The function returns 1 when a row in the SalesRep column is the same as the user executing the query (@SalesRep = USER_NAME())
+       or if the user executing the query is the Manager user (USER_NAME() = 'InventoryManager').
+       */
+end
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[SP_RLS_InventoryManager] AS
+EXECUTE AS USER = 'InventoryManager';  
+SELECT * FROM [MFG-FactSales];
+revert;
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[SP_RLS_SalesStaffMiami] AS
+EXECUTE AS USER = 'SalesStaffMiami' 
+SELECT * FROM [MFG-FactSales];
+revert;
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROC [dbo].[SP_RLS_SalesStaffSanDiego] AS
+EXECUTE AS USER = 'SalesStaffSanDiego'; 
+SELECT * FROM [MFG-FactSales];
+revert;
 
 
 
