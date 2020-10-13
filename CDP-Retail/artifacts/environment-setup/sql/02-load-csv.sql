7f18deda6569248a8416997696bb6cf5264e66b4
KmrShiv <69001915+KmrShiv@users.noreply.github.com>
2020-10-13 16:13:16 +0000

    Update 02-load-csv.sql

--- CDP-Retail/artifacts/environment-setup/sql/02-load-csv.sql
+++ CDP-Retail/artifacts/environment-setup/sql/02-load-csv.sql
@@ -1,5 +1,5 @@
 COPY INTO #TABLE_NAME# 
-FROM 'https://asaexpdatalakecdpu.blob.core.windows.net/customcsv/Retail Scenario Dataset/#CSV_FILE_NAME#.csv'
+FROM 'https://asaexpdatalakecdpu.blob.core.windows.net/cdp/csv/#CSV_FILE_NAME#.csv'
 WITH (
 	FILE_TYPE = 'CSV',
 	FIRSTROW = #DATA_START_ROW_NUMBER# 
