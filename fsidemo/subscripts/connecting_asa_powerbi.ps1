6b6c62ba261e6894adf4309a32441f898c78dab9
priyankamali <Priyanka.Mali@Xoriant.com>
2021-10-07 17:52:18 +0530

    Create connecting_asa_powerbi.ps1

---
+++ fsidemo/subscripts/connecting_asa_powerbi.ps1
@@ -0,0 +1,51 @@
+function RefreshTokens()
+{
+    #Copy external blob content
+    $global:powerbitoken = ((az account get-access-token --resource https://analysis.windows.net/powerbi/api) | ConvertFrom-Json).accessToken
+    $global:synapseToken = ((az account get-access-token --resource https://dev.azuresynapse.net) | ConvertFrom-Json).accessToken
+    $global:graphToken = ((az account get-access-token --resource https://graph.microsoft.com) | ConvertFrom-Json).accessToken
+    $global:managementToken = ((az account get-access-token --resource https://management.azure.com) | ConvertFrom-Json).accessToken
+}
+
+#should auto for this.
+az login
+
+#for powershell...
+Connect-AzAccount -DeviceCode
+
+#if they have many subs...
+$subs = Get-AzSubscription | Select-Object -ExpandProperty Name
+
+if($subs.GetType().IsArray -and $subs.length -gt 1)
+{
+    $subOptions = [System.Collections.ArrayList]::new()
+    for($subIdx=0; $subIdx -lt $subs.length; $subIdx++)
+    {
+        $opt = New-Object System.Management.Automation.Host.ChoiceDescription "$($subs[$subIdx])", "Selects the $($subs[$subIdx]) subscription."   
+        $subOptions.Add($opt)
+    }
+    $selectedSubIdx = $host.ui.PromptForChoice('Enter the desired Azure Subscription for this lab','Copy and paste the name of the subscription to make your choice.', $subOptions.ToArray(),0)
+    $selectedSubName = $subs[$selectedSubIdx]
+    Write-Host "Selecting the $selectedSubName subscription"
+    Select-AzSubscription -SubscriptionName $selectedSubName
+    az account set --subscription $selectedSubName
+}
+
+#Getting User Inputs
+$rgName = read-host "Enter the resource Group Name";
+$init =  (Get-AzResourceGroup -Name $rgName).Tags["DeploymentId"]
+$random =  (Get-AzResourceGroup -Name $rgName).Tags["UniqueId"]
+$wsId =  (Get-AzResourceGroup -Name $rgName).Tags["WsId"]
+$suffix = "$random-$init"
+$asa_name_fsi = "fsiasa-$suffix"
+
+RefreshTokens
+Write-Host "----ASA Powerbi Connection-----"
+#connecting asa and powerbi
+$principal=az resource show -g $rgName -n $asa_name_fsi --resource-type "Microsoft.StreamAnalytics/streamingjobs"|ConvertFrom-Json
+$principalId=$principal.identity.principalId
+Add-PowerBIWorkspaceUser -WorkspaceId $wsId -PrincipalId $principalId -PrincipalType App -AccessRight Admin
+
+#start ASA
+Write-Host "----Starting ASA-----"
+Start-AzStreamAnalyticsJob -ResourceGroupName $rgName -Name $asa_name_fsi -OutputStartMode 'JobStartTime'
