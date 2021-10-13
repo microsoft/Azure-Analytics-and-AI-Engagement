11a72f854fcd33f89b53ae28ce3e4c60dd516483
Ranavikrant <33507675+xor-ranavikrant@users.noreply.github.com>
2021-10-13 23:53:43 +0530

    rls sp

--- fsidemo/artifacts/sqlscripts/tableschema.sql
+++ fsidemo/artifacts/sqlscripts/tableschema.sql
@@ -3890,3 +3890,27 @@ SELECT c.name, tbl.name as table_name, c.is_masked, c.masking_function
 FROM sys.masked_columns AS c  
 JOIN sys.tables AS tbl   ON c.[object_id] = tbl.[object_id]  WHERE 
 is_masked = 1 and tbl.name='CustomerInfo';
+
+CREATE PROC [dbo].[Sp_FinanceRLS] AS 
+EXECUTE AS USER = 'MarketingOwner'
+Begin	
+	-- After creating the users, read access is provided to all three users on FactSales table
+	GRANT SELECT ON [Finance-FactSales] TO HeadOfFinancialIntelligence, BusinessAnalystMiami, BusinessAnalystNYC;  
+
+	IF EXISts (SELECT 1 FROM sys.security_predicates sp where sp.predicate_definition='([Security].[fn_securitypredicate]([Designation]))')
+	BEGIN
+		DROP SECURITY POLICY SalesFilter;
+		DROP FUNCTION Security.fn_securitypredicate;
+	END
+	
+	IF  EXISTS (SELECT * FROM sys.schemas where name='Security')
+	BEGIN	
+	DROP SCHEMA Security;
+	End
+	
+	/* Moving ahead, we Create a new schema, and an inline table-valued function. 
+	The function returns 1 when a row in the SalesRep column is the same as the user executing the query (@SalesRep = USER_NAME())
+	or if the user executing the query is the Manager user (USER_NAME() = 'HeadOfFinancialIntelligence').
+	*/
+end
+REVERT;
