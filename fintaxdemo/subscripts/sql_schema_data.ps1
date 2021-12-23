7fb29c1c0267bedd74dbb963a357dcadc0d74574
priyankamali <Priyanka.Mali@Xoriant.com>
2021-12-23 15:55:36 +0530

    updated sql_schema_data subscript.

--- fintaxdemo/subscripts/sql_schema_data.ps1
+++ fintaxdemo/subscripts/sql_schema_data.ps1
@@ -65,13 +65,7 @@ if($dataLakeAccountName.length -gt 24)
 {
 $dataLakeAccountName = $dataLakeAccountName.substring(0,24)
 }
-$db_graph_fintax_name = "db-acc-graph-fintax-$concatString"
-if($db_graph_fintax_name.length -gt 43 )
-{
-$db_graph_fintax_name = $db_graph_fintax_name.substring(0,43)
-}
 
-Add-Content log.txt "------sql schema-----"
 Write-Host "----Sql Schema------"
 RefreshTokens
 #creating sql schema
@@ -82,7 +76,12 @@ $sqlEndpoint="$($synapseWorkspaceName).sql.azuresynapse.net"
 $result=Invoke-SqlCmd -Query $sqlQuery -ServerInstance $sqlEndpoint -Database $sqlPoolName -Username $sqlUser -Password $sqlPassword
 Add-Content log.txt $result
 
-#$sqlQuery = Get-Content -Raw -Path "$($SQLScriptsPath)/sqluser.sql"
+$sqlQuery = Get-Content -Raw -Path "$($SQLScriptsPath)/storedprocedures.sql"
+$sqlEndpoint="$($synapseWorkspaceName).sql.azuresynapse.net"
+$result=Invoke-SqlCmd -Query $sqlQuery -ServerInstance $sqlEndpoint -Database $sqlPoolName -Username $sqlUser -Password $sqlPassword
+Add-Content log.txt $result
+
+$sqlQuery = Get-Content -Raw -Path "$($SQLScriptsPath)/sqluser.sql"
 (Get-Content -path "$($SQLScriptsPath)/sqluser.sql" -Raw) | Foreach-Object { $_ `
                 -replace '#SQL_PASSWORD#', $sqlPassword`		
         } | Set-Content -Path "$($SQLScriptsPath)/sqluser.sql"		
@@ -91,116 +90,84 @@ $sqlEndpoint="$($synapseWorkspaceName).sql.azuresynapse.net"
 $result=Invoke-SqlCmd -Query $sqlQuery -ServerInstance $sqlEndpoint -Database 'master' -Username $sqlUser -Password $sqlPassword
 Add-Content log.txt $result
 
-$sqlQuery="CREATE USER [MarketingOfficer] FOR LOGIN [MarketingOfficer] WITH DEFAULT_SCHEMA=[dbo]"
-$sqlEndpoint="$($synapseWorkspaceName).sql.azuresynapse.net"
+$sqlQuery = Get-Content -Raw -Path "$($SQLScriptsPath)/sql_user_fintax.sql"
 $result=Invoke-SqlCmd -Query $sqlQuery -ServerInstance $sqlEndpoint -Database $sqlPoolName -Username $sqlUser -Password $sqlPassword
+Add-Content log.txt $result
+
+ $sqlQuery="GRANT SELECT ON [Fact-Invoices] TO  TaxAuditor, TaxAuditSupervisor, TaxAuditorSurDatum, TaxAuditorStatiso;"
+ $result=Invoke-SqlCmd -Query $sqlQuery -ServerInstance $sqlEndpoint -Database $sqlPoolName -Username $sqlUser -Password $sqlPassword
+
+ $sqlQuery="GRANT SELECT ON [FactInvoices] TO  TaxAuditor, TaxAuditSupervisor, TaxAuditorSurDatum, TaxAuditorStatiso;"
+ $result=Invoke-SqlCmd -Query $sqlQuery -ServerInstance $sqlEndpoint -Database $sqlPoolName -Username $sqlUser -Password $sqlPassword
+
+ $sqlQuery="GRANT SELECT ON [FactInvoicesData] TO AntiCorruptionUnitHead;"
+ $result=Invoke-SqlCmd -Query $sqlQuery -ServerInstance $sqlEndpoint -Database $sqlPoolName -Username $sqlUser -Password $sqlPassword
 
 $sqlQuery  = "CREATE DATABASE FintaxSqlOnDemand"
 $sqlEndpoint = "$($synapseWorkspaceName)-ondemand.sql.azuresynapse.net"
 $result=Invoke-SqlCmd -Query $sqlQuery -ServerInstance $sqlEndpoint -Database master -Username $sqlUser -Password $sqlPassword
-Add-Content log.txt $result	
   
-#uploading Sql Scripts
-Add-Content log.txt "-----------uploading Sql Scripts-----------------"
-Write-Host "----Sql Scripts------"
-RefreshTokens
-$scripts=Get-ChildItem "../artifacts/sqlscripts" | Select BaseName
-$TemplatesPath="../artifacts/templates";	
-
-foreach ($name in $scripts) 
-{
-    if ($name.BaseName -eq "tableschema" )
-    {
-        continue;
-    }
-
-    $item = Get-Content -Raw -Path "$($TemplatesPath)/sql_script.json"
-    $item = $item.Replace("#SQL_SCRIPT_NAME#", $name.BaseName)
-    $item = $item.Replace("#SQL_POOL_NAME#", $sqlPoolName)
-    $jsonItem = ConvertFrom-Json $item 
-    $ScriptFileName="../artifacts/sqlscripts/"+$name.BaseName+".sql"
-    
-    $query = Get-Content -Raw -Path $ScriptFileName -Encoding utf8
-    $query = $query.Replace("#STORAGE_ACCOUNT#", $dataLakeAccountName)
-    $query = $query.Replace("#STORAGE_ACCOUNT_NAME#", $dataLakeAccountName)
-    $query = $query.Replace("#COSMOS_ACCOUNT#", $db_graph_fintax_name)
-    # $query = $query.Replace("#COSMOS_KEY#", $cosmos_account_key)
-
-    $query = $query.Replace("#LOCATION#", $location)
-	
-    if ($Parameters -ne $null) 
-    {
-        foreach ($key in $Parameters.Keys) 
-        {
-            $query = $query.Replace("#$($key)#", $Parameters[$key])
-        }
-    }
-
-    Write-Host "Uploading Sql Script : $($name.BaseName)"
-    $query = ConvertFrom-Json (ConvertTo-Json $query)
-    $jsonItem.properties.content.query = $query
-    $item = ConvertTo-Json $jsonItem -Depth 100
-    $uri = "https://$($synapseWorkspaceName).dev.azuresynapse.net/sqlscripts/$($name.BaseName)?api-version=2019-06-01-preview"
-    $result = Invoke-RestMethod  -Uri $uri -Method PUT -Body $item -Headers @{ Authorization="Bearer $synapseToken" } -ContentType "application/json"
-    Add-Content log.txt $result
-}
-
-Add-Content log.txt "------uploading sql data------"
 Write-Host  "-------------Uploading Sql Data ---------------"
 RefreshTokens
 #uploading sql data
 $dataTableList = New-Object System.Collections.ArrayList
 
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"AnnualTaxGapOverview"}} , @{Name = "TABLE_NAME"; Expression = {"AnnualTaxGapOverview"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"All_Data"}} , @{Name = "TABLE_NAME"; Expression = {"All_Data"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"AnnualTaxRevenueAnalytics"}} , @{Name = "TABLE_NAME"; Expression = {"AnnualTaxRevenueAnalytics"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Color"}} , @{Name = "TABLE_NAME"; Expression = {"Color"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Campaign_Analytics"}} , @{Name = "TABLE_NAME"; Expression = {"Campaign_Analytics"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Corruption_Data"}} , @{Name = "TABLE_NAME"; Expression = {"Corruption_Data"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"CustomerInfo"}} , @{Name = "TABLE_NAME"; Expression = {"CustomerInfo"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Customer"}} , @{Name = "TABLE_NAME"; Expression = {"Customer"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
 $temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Fact-dailytaxdetails"}} , @{Name = "TABLE_NAME"; Expression = {"Fact-dailytaxdetails"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"fact-monthly"}} , @{Name = "TABLE_NAME"; Expression = {"fact-monthly"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"FactInvoicedetailsfinal"}} , @{Name = "TABLE_NAME"; Expression = {"FactInvoicedetailsfinal"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$list = $dataTableList.Add($temp)
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"FactInvoicedetailsfinals"}} , @{Name = "TABLE_NAME"; Expression = {"FactInvoicedetailsfinals"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$list = $dataTableList.Add($temp)
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"FactInvoicedetailsIDpTA"}} , @{Name = "TABLE_NAME"; Expression = {"FactInvoicedetailsIDpTA"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$list = $dataTableList.Add($temp)
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"FactInvoices"}} , @{Name = "TABLE_NAME"; Expression = {"FactInvoices"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$list = $dataTableList.Add($temp)
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Fact-Invoices"}} , @{Name = "TABLE_NAME"; Expression = {"Fact-Invoices"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$list = $dataTableList.Add($temp)
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"FactInvoicesData"}} , @{Name = "TABLE_NAME"; Expression = {"FactInvoicesData"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
 $temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"fact-pbiMonthlyTaxDetails"}} , @{Name = "TABLE_NAME"; Expression = {"fact-pbiMonthlyTaxDetails"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"fact-pbiMonthlyTaxDetails_08252021"}} , @{Name = "TABLE_NAME"; Expression = {"fact-pbiMonthlyTaxDetails_08252021"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
-$list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"fact-pbiMonthlyTaxDetails_09132021"}} , @{Name = "TABLE_NAME"; Expression = {"fact-pbiMonthlyTaxDetails_09132021"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
-$list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"fact-pbiMonthlyTaxDetails_PST_09132021"}} , @{Name = "TABLE_NAME"; Expression = {"fact-pbiMonthlyTaxDetails_PST_09132021"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
-$list = $dataTableList.Add($temp)
 $temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Fact-Taxpayersatisfactiondetail"}} , @{Name = "TABLE_NAME"; Expression = {"Fact-Taxpayersatisfactiondetail"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Finance-FactSales"}} , @{Name = "TABLE_NAME"; Expression = {"Finance-FactSales"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Fintaxtransaction"}} , @{Name = "TABLE_NAME"; Expression = {"Fintaxtransaction"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$list = $dataTableList.Add($temp)
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Industry"}} , @{Name = "TABLE_NAME"; Expression = {"Industry"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$list = $dataTableList.Add($temp)
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"iot-foottraffic-data"}} , @{Name = "TABLE_NAME"; Expression = {"iot-foottraffic-data"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$list = $dataTableList.Add($temp)
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Location"}} , @{Name = "TABLE_NAME"; Expression = {"Location"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
 $temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"pbiTaxCustSatMonthly"}} , @{Name = "TABLE_NAME"; Expression = {"pbiTaxCustSatMonthly"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
 $temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"pbiTaxDetailMonthly"}} , @{Name = "TABLE_NAME"; Expression = {"pbiTaxDetailMonthly"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"TaxGapByBusinessType"}} , @{Name = "TABLE_NAME"; Expression = {"TaxGapByBusinessType"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"Source"}} , @{Name = "TABLE_NAME"; Expression = {"Source"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"TaxGapByRegion"}} , @{Name = "TABLE_NAME"; Expression = {"TaxGapByRegion"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"StagingFintaxtransaction"}} , @{Name = "TABLE_NAME"; Expression = {"StagingFintaxtransaction"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
 $temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"TaxpayerSatisfactionMetrics"}} , @{Name = "TABLE_NAME"; Expression = {"TaxpayerSatisfactionMetrics"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"UpcomingComplianceEvents"}} , @{Name = "TABLE_NAME"; Expression = {"UpcomingComplianceEvents"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"temp"}} , @{Name = "TABLE_NAME"; Expression = {"temp"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
+$list = $dataTableList.Add($temp)
+$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"TRF-Social-Data"}} , @{Name = "TABLE_NAME"; Expression = {"TRF-Social-Data"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
 $temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"VAT_Daily"}} , @{Name = "TABLE_NAME"; Expression = {"VAT_Daily"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
-$list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"VAT_Daily_1"}} , @{Name = "TABLE_NAME"; Expression = {"VAT_Daily_1"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
-$list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"VAT_Daily_08312021"}} , @{Name = "TABLE_NAME"; Expression = {"VAT_Daily_08312021"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
-$list = $dataTableList.Add($temp)
-$temp = "" | select-object @{Name = "CSV_FILE_NAME"; Expression = {"VAT_Daily_09022021"}} , @{Name = "TABLE_NAME"; Expression = {"VAT_Daily_09022021"}}, @{Name = "DATA_START_ROW_NUMBER"; Expression = {2}}
 $list = $dataTableList.Add($temp)
 
 $sqlEndpoint="$($synapseWorkspaceName).sql.azuresynapse.net"
 foreach ($dataTableLoad in $dataTableList) {
     Write-output "Loading data for $($dataTableLoad.TABLE_NAME)"
     $sqlQuery = Get-Content -Raw -Path "../artifacts/templates/load_csv.sql"
-    $sqlquery = $sqlquery.Replace("#STORAGE_ACCOUNT#", $dataLakeAccountName)
+    $sqlQuery = $sqlQuery.Replace("#STORAGE_ACCOUNT#", $dataLakeAccountName)
     $Parameters =@{
             CSV_FILE_NAME = $dataTableLoad.CSV_FILE_NAME
             TABLE_NAME = $dataTableLoad.TABLE_NAME
