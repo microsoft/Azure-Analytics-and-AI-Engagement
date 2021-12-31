7aa0d917b5b7017828f01106535f14286015eafc
priyankamali <Priyanka.Mali@Xoriant.com>
2021-12-31 16:14:46 +0530

    Update powerbi_subscript.ps1

--- fintaxdemo/subscripts/powerbi_subscript.ps1
+++ fintaxdemo/subscripts/powerbi_subscript.ps1
@@ -103,15 +103,6 @@ foreach($name in $reports)
 }
 Start-Sleep -s 60
 
-$url = "https://api.powerbi.com/v1.0/myorg/groups/$wsId/reports"
-$pbiResult = Invoke-RestMethod -Uri $url -Method GET -ContentType "application/json" -Headers @{ Authorization="Bearer $powerbitoken" } -ea SilentlyContinue;
-
-foreach($r in $pbiResult.value)
-{
-    $report = $reportList | where {$_.Name -eq $r.name}
-    $report.ReportId = $r.id;
-}
-
 ##Establish powerbi reports dataset connections
 Write-Host "--------- PBI connections update---------"	
 
