0285c9ee636ccb4e01fde9eaace39b7f51531d47
priyankamali <Priyanka.Mali@Xoriant.com>
2022-02-18 15:46:05 +0530

    Update fsiSetup.ps1

--- fsidemo/fsiSetup.ps1
+++ fsidemo/fsiSetup.ps1
@@ -240,7 +240,16 @@ $cosmos_account_key=$cosmos_account_key.primarymasterkey
 New-Item log.txt
 
 Install-Module -Name MicrosoftPowerBIMgmt -Force
-Login-PowerBI
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","I have enough permissions for PowerBI login."
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No","I will run PowerBI setup seperately."
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+$title = "PowerBI login"
+$message = " (Type [Y] for Yes or [N] for No and press enter)"
+$result = $host.ui.PromptForChoice($title, $message, $options, 1)
+if($result -eq 0)
+{
+ Login-PowerBI 
+}
 
 RefreshTokens
 Write-Host "-----Enable Transparent Data Encryption----------"
