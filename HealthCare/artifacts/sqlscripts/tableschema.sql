f6e75ba5e25b5b5af77a1ff8fde1260f045c8fbb
xor-ranavikrant <33507675+xor-ranavikrant@users.noreply.github.com>
2022-03-11 17:33:08 +0530

    Updated Stored proc

--- HealthCare/artifacts/sqlscripts/tableschema.sql
+++ HealthCare/artifacts/sqlscripts/tableschema.sql
@@ -1126,10 +1126,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE PROC [dbo].[CLS_ChiefOperatingManager] AS 
 Revert;
-GRANT SELECT ON Campaign_Analytics TO ChiefOperatingManager;  --Full access to all columns.
+GRANT SELECT ON Campaign_Analytics_New TO ChiefOperatingManager;  --Full access to all columns.
 -- Step:6 Let us check if our ChiefOperatingManager user can see all the information that is present. Assign Current User As 'CEO' and the execute the query
 EXECUTE AS USER ='ChiefOperatingManager'
-select * from Campaign_Analytics
+select * from Campaign_Analytics_New
 GO
 /****** Object:  StoredProcedure [dbo].[CLS_DAM_AC_New]    Script Date: 12/22/2020 12:52:12 PM ******/
 SET ANSI_NULLS ON
