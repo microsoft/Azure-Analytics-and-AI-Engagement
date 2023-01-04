acbff14708dd0bc7e835a9a6547f12a344578450
AdnanSharique <48666706+AdnanSharique@users.noreply.github.com>
2023-01-04 12:37:03 +0530

    updated subscripts and modular scripts

--- retail/subscripts/powerbi_subscript.ps1
+++ retail/subscripts/powerbi_subscript.ps1
@@ -18,7 +18,7 @@ $subs = Get-AzSubscription | Select-Object -ExpandProperty Name
 
 if($subs.GetType().IsArray -and $subs.length -gt 1)
 {
-    $subOptions = [System.Collections.ArrayList]::new()
+   $subOptions = [System.Collections.ArrayList]::new()
     for($subIdx=0; $subIdx -lt $subs.length; $subIdx++)
     {
         $opt = New-Object System.Management.Automation.Host.ChoiceDescription "$($subs[$subIdx])", "Selects the $($subs[$subIdx]) subscription."   
@@ -26,9 +26,24 @@ if($subs.GetType().IsArray -and $subs.length -gt 1)
     }
     $selectedSubIdx = $host.ui.PromptForChoice('Enter the desired Azure Subscription for this lab','Copy and paste the name of the subscription to make your choice.', $subOptions.ToArray(),0)
     $selectedSubName = $subs[$selectedSubIdx]
-    Write-Host "Selecting the $selectedSubName subscription"
+    Write-Host "Selecting the subscription : $selectedSubName "
+	$title    = 'Subscription selection'
+	$question = 'Are you sure you want to select this subscription for this lab?'
+	$choices  = '&Yes', '&No'
+	$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+	if($decision -eq 0)
+	{
     Select-AzSubscription -SubscriptionName $selectedSubName
     az account set --subscription $selectedSubName
+	}
+	else
+	{
+	$selectedSubIdx = $host.ui.PromptForChoice('Enter the desired Azure Subscription for this lab','Copy and paste the name of the subscription to make your choice.', $subOptions.ToArray(),0)
+    $selectedSubName = $subs[$selectedSubIdx]
+    Write-Host "Selecting the subscription : $selectedSubName "
+	Select-AzSubscription -SubscriptionName $selectedSubName
+    az account set --subscription $selectedSubName
+	}
 }
 
 #Getting User Inputs
