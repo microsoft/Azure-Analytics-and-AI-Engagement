7e3c5a16eb97ab8601fd134a4a5b1c76e9cf980f
AdnanSharique <48666706+AdnanSharique@users.noreply.github.com>
2023-01-05 20:32:26 +0530

    updated main script and mssql subscript

--- Manufacturing/automation/manufacturingSetup-shell.ps1
+++ Manufacturing/automation/manufacturingSetup-shell.ps1
@@ -1157,20 +1157,20 @@ $result = Invoke-RestMethod -Uri $url -Method GET -ContentType "application/json
 
         #update all th report ids in the poc web app...
 $ht = new-object system.collections.hashtable
-$ht.add("#REPORT_SQL_DASHBOARD_BEFORE_ID#", $($reportList | where {$_.Name -eq "1_Billion rows demo"}).ReportId)
-$ht.add("#REPORT_SQL_DASHBOARD_DURING_ID#", $($reportList | where {$_.Name -eq "3_MFG Dynamic Data Masking (Azure Synapse)"}).ReportId)
-$ht.add("#REPORT_SQL_DASHBOARD_AFTER_ID#", $($reportList | where {$_.Name -eq "4_MFG Column Level Security (Azure Synapse)"}).ReportId)
-$ht.add("#REPORT_DASHBOARD_AFTER_ID#", $($reportList | where {$_.Name -eq "5_MFG Row Level Security (Azure Synapse)"}).ReportId)
-$ht.add("#REPORT_ANOMALY_ID#", $($reportList | where {$_.Name -eq "anomaly detection with images"}).ReportId)
-$ht.add("#REPORT_CAMPAIGN_ID#", $($reportList | where {$_.Name -eq "Campaign - Option C"}).ReportId)
-$ht.add("#REPORT_FACTORY_ID#", $($reportList | where {$_.Name -eq "Factory-Overview - Option A"}).ReportId)
-$ht.add("#REPORT_FINANCE_ID#", $($reportList | where {$_.Name -eq "1_Billion rows demo"}).ReportId)
-$ht.add("#REPORT_GLOBALBING_ID#", $($reportList | where {$_.Name -eq "VP-Global-Overview"}).ReportId)
-$ht.add("#REPORT_SAFETY_ID#", $($reportList | where {$_.Name -eq "Factory-Overview - Option A"}).ReportId)
-$ht.add("#REPORT_MACHINE_ID#", $($reportList | where {$_.Name -eq "Equipment View Report"}).ReportId)
-$ht.add("#REPORT_MACHINE_ANOMOLY_ID#", $($reportList | where {$_.Name -eq "anomaly detection with images"}).ReportId)
-$ht.add("#REPORT_HTAP_ID#", $($reportList | where {$_.Name -eq "6_Production Quality- HTAP Synapse Link"}).ReportId)
-$ht.add("#REPORT_SALES_CAMPAIGN_ID#", $($reportList | where {$_.Name -eq "Campaign Sales Operations"}).ReportId)
+$ht.add("#REPORT_SQL_DASHBOARD_BEFORE_ID#", $($reportList | where {$_.name -eq "1_Billion rows demo"}).id)
+$ht.add("#REPORT_SQL_DASHBOARD_DURING_ID#", $($reportList | where {$_.name -eq "3_MFG Dynamic Data Masking (Azure Synapse)"}).id)
+$ht.add("#REPORT_SQL_DASHBOARD_AFTER_ID#", $($reportList | where {$_.name -eq "4_MFG Column Level Security (Azure Synapse)"}).id)
+$ht.add("#REPORT_DASHBOARD_AFTER_ID#", $($reportList | where {$_.name -eq "5_MFG Row Level Security (Azure Synapse)"}).id)
+$ht.add("#REPORT_ANOMALY_ID#", $($reportList | where {$_.name -eq "anomaly detection with images"}).id)
+$ht.add("#REPORT_CAMPAIGN_ID#", $($reportList | where {$_.name -eq "Campaign - Option C"}).id)
+$ht.add("#REPORT_FACTORY_ID#", $($reportList | where {$_.name -eq "Factory-Overview - Option A"}).id)
+$ht.add("#REPORT_FINANCE_ID#", $($reportList | where {$_.name -eq "1_Billion rows demo"}).id)
+$ht.add("#REPORT_GLOBALBING_ID#", $($reportList | where {$_.name -eq "VP-Global-Overview"}).id)
+$ht.add("#REPORT_SAFETY_ID#", $($reportList | where {$_.name -eq "Factory-Overview - Option A"}).id)
+$ht.add("#REPORT_MACHINE_ID#", $($reportList | where {$_.name -eq "Equipment View Report"}).id)
+$ht.add("#REPORT_MACHINE_ANOMOLY_ID#", $($reportList | where {$_.name -eq "anomaly detection with images"}).id)
+$ht.add("#REPORT_HTAP_ID#", $($reportList | where {$_.name -eq "6_Production Quality- HTAP Synapse Link"}).id)
+$ht.add("#REPORT_SALES_CAMPAIGN_ID#", $($reportList | where {$_.name -eq "Campaign Sales Operations"}).id)
 $ht.add("#WWI_SITE_NAME#", $wideworldimporters_app_service_name)
 $ht.add("#STORAGE_ACCOUNT#", $dataLakeAccountName)
 $ht.add("#WORKSPACE_ID#", $wsId)
@@ -1451,7 +1451,7 @@ RefreshTokens
 #create aml workspace
 az extension add -n azure-cli-ml
 
-az ml workspace create -w $amlworkspacename -g $rgName
+az ml workspace create -n $amlworkspacename -g $rgName
 
 #attach a folder to set resource group and workspace name (to skip passing ws and rg in calls after this line)
 az ml folder attach -w $amlworkspacename -g $rgName -e aml
