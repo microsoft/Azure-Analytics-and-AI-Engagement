6658a5943cdb19eb84029e3b3a276a82cfcbe1aa
AdnanSharique <48666706+AdnanSharique@users.noreply.github.com>
2023-05-12 15:35:41 +0530

    updated images and chatBot.sql

--- healthcare2/artifacts/sqlscripts/chatBot.sql
+++ healthcare2/artifacts/sqlscripts/chatBot.sql
@@ -4,5 +4,5 @@ CREATE TABLE [dbo].[Chat Bot]
 	[URL 1] [varchar](400)  NULL
 )
 
-INSERT INTO Chat Bot ([URL], [URL 1])
+INSERT INTO [Chat Bot] ([URL], [URL 1])
 VALUES ('<!DOCTYPE html><html><body><iframe sandbox="allow-same-origin allow-scripts" style="width: 375px; height: 608px; border: 0px;" src="#OPEN_AI_ENDPOINT#" title="Chat Bot"></iframe></body></html>', '#OPEN_AI_ENDPOINT#');
