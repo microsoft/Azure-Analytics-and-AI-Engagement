4b15c49b30b73a9ae0283989f5ff3795466d5569
Zubair8899 <98596355+Zubair8899@users.noreply.github.com>
2025-06-24 12:37:11 +0530

    Update mediaSetup.ps1

--- mediademo/mediaSetup.ps1
+++ mediademo/mediaSetup.ps1
@@ -122,21 +122,7 @@ if($subs.GetType().IsArray -and $subs.length -gt 1)
     az account set --subscription $selectedSubName
 }
 
-#getting user details
 
-$response=az ad signed-in-user show | ConvertFrom-Json
-$date=get-date
-$demoType="Media"
-$body= '{"demoType":"#demoType#","userPrincipalName":"#userPrincipalName#","displayName":"#displayName#","companyName":"#companyName#","mail":"#mail#","date":"#date#"}'
-$body = $body.Replace("#userPrincipalName#", $response.userPrincipalName)
-$body = $body.Replace("#displayName#", $response.displayName)
-$body = $body.Replace("#companyName#", $response.companyName)
-$body = $body.Replace("#mail#", $response.mail)
-$body = $body.Replace("#date#", $date)
-$body = $body.Replace("#demoType#", $demoType)
-
-$uri ="https://registerddibuser.azurewebsites.net/api/registeruser?code=pTrmFDqp25iVSxrJ/ykJ5l0xeTOg5nxio9MjZedaXwiEH8oh3NeqMg=="
-$result = Invoke-RestMethod  -Uri $uri -Method POST -Body $body -Headers @{} -ContentType "application/json"
 
 #Getting User Inputs
 $rgName = read-host "Enter the resource Group Name";
